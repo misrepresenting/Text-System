ec0a8d693b81701af435332abea82c0a8548af38
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-01-24 16:47:21 -0500

    Update ScrollingFix.lua

--- InputChanged/ScrollingFix.lua
+++ InputChanged/ScrollingFix.lua
@@ -1,1 +1,16 @@
+-- LineCounter
 
+-- vars
+local TextService = game:GetService("TextService");
+local RunService = game:GetService("RunService");
+local UserInputService = game:GetService("UserInputService");
+
+-- functions
+local function Main(Input, TextBox)
+	if (Input.UserInputType == Enum.UserInputType.MouseWheel) then
+		TextBox:ReleaseFocus();
+	end
+end
+
+-- main
+return Main;
