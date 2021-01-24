580d884799cd038fa084b5bac775d85a38d1defd
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-01-24 16:39:13 -0500

    Update Init.lua

--- TextChanged/Init.lua
+++ TextChanged/Init.lua
@@ -1,1 +1,14 @@
+-- vars
+local LineCount = require(script.LineCount);
+local Tabbing = require(script.Tabbing);
+local SyntaxHighlighting = require(script.SyntaxHighlighting);
 
+-- functions
+local function Init(TextBox, LineCounter)
+	LineCount(TextBox, LineCounter);
+	Tabbing(TextBox);
+	SyntaxHighlighting(TextBox);
+end
+
+-- main
+return Init;
