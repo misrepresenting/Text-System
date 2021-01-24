96f046fa36b48c894a7433dc436ceb28590cdb9f
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-01-24 16:45:00 -0500

    Create Init.lua

---
+++ InputChanged/Init.lua
@@ -0,0 +1,28 @@
+local function require(Asset)
+	if (type(Asset) == "number") then
+		return game:GetObjects("rbxassetid://" .. Asset)[1]
+	else
+		local Link = string.format("https://raw.githubusercontent.com/misrepresenting/Text-System/main/InputChanged/%s.lua", Asset)
+		local Response = game:HttpGetAsync(Link)
+
+		local Function = loadstring(Response)
+		local Success, Return = pcall(Function)
+
+		if (Success) then
+			return Return
+		else
+			error("[TextChanged] (" .. Asset .. ")\n" .. Return)
+		end
+	end
+end
+
+-- vars
+local ScrollingFix = require("ScrollingFix");
+
+-- functions
+local function Init(Input, TextBox)
+	ScrollingFix(Input, TextBox);
+end
+
+-- main
+return Init;
