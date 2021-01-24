7eaef1e348f05e95d0b661a8d8f5a68deae4ce08
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-01-24 16:50:56 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,7 +2,7 @@ local function require(Asset)
 	if (type(Asset) == "number") then
 		return game:GetObjects("rbxassetid://" .. Asset)[1]
 	else
-		local Link = string.format("https://raw.githubusercontent.com/misrepresenting/Text-System/main/%s.lua", Asset)
+		local Link = string.format("https://raw.githubusercontent.com/misrepresenting/Text-System/main/%s/Init.lua", Asset)
 		local Response = game:HttpGetAsync(Link)
 
 		local Function = loadstring(Response)
