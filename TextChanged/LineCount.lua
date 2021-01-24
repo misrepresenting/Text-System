c32c6eefa0f77eba890012da15c0655d901a5ff4
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-01-24 16:39:27 -0500

    Update LineCount.lua

--- TextChanged/LineCount.lua
+++ TextChanged/LineCount.lua
@@ -1,1 +1,22 @@
+-- LineCounter
 
+-- vars
+local TextService = game:GetService("TextService");
+local RunService = game:GetService("RunService");
+local UserInputService = game:GetService("UserInputService");
+
+-- functions
+local function Main(TextBox, LineCounter)
+	local Text = TextBox.Text;
+	local LineCount = #string.split(Text, "\n");
+	local LineCountString = "";
+	
+	for i = 1, LineCount do
+		LineCountString ..= i .. "\n";
+	end
+	
+	LineCounter.Text = LineCountString;
+end
+
+-- main
+return Main;
