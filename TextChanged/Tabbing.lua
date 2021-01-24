ab6182d68bf3230c9b2862a7520b6c58e715ae96
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-01-24 16:39:56 -0500

    Update Tabbing.lua

--- TextChanged/Tabbing.lua
+++ TextChanged/Tabbing.lua
@@ -1,1 +1,21 @@
+-- Resize
 
+-- vars
+local TextService = game:GetService("TextService");
+local RunService = game:GetService("RunService");
+local UserInputService = game:GetService("UserInputService");
+
+
+-- functions
+local function Main(TextBox)
+	local Text = TextBox.Text;
+	local PrevChar = string.sub(Text, TextBox.CursorPosition - 1, TextBox.CursorPosition - 1);
+
+	if (PrevChar == "\t") then
+		TextBox.Text = string.sub(Text, 1, TextBox.CursorPosition - 2) .. "    " .. string.sub(Text, TextBox.CursorPosition, #Text);
+		TextBox.CursorPosition = TextBox.CursorPosition + 3;
+	end
+end
+
+-- main
+return Main;
