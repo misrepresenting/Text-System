81bfb5bb6697c30fae2580788f330107e8e13bc7
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-01-24 16:44:16 -0500

    Update Init.lua

--- TextChanged/Init.lua
+++ TextChanged/Init.lua
@@ -1,7 +1,25 @@
+local function require(Asset)
+	if (type(Asset) == "number") then
+		return game:GetObjects("rbxassetid://" .. Asset)[1]
+	else
+		local Link = string.format("https://raw.githubusercontent.com/misrepresenting/Text-System/main/TextChanged/%s.lua", Asset)
+		local Response = game:HttpGetAsync(Link)
+
+		local Function = loadstring(Response)
+		local Success, Return = pcall(Function)
+
+		if (Success) then
+			return Return
+		else
+			error("[TextChanged] (" .. Asset .. ")\n" .. Return)
+		end
+	end
+end
+
 -- vars
-local LineCount = require(script.LineCount);
-local Tabbing = require(script.Tabbing);
-local SyntaxHighlighting = require(script.SyntaxHighlighting);
+local LineCount = require("LineCount");
+local Tabbing = require("Tabbing");
+local SyntaxHighlighting = require("SyntaxHighlighting");
 
 -- functions
 local function Init(TextBox, LineCounter)
